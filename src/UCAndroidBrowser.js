90516b280a330e78dfc27c29ee06c5e4e69b8d94
fage <924053302@qq.com>
2017-06-17 23:00:25 +0800

    修复安卓uc浏览器e下title和des互换问题 无法呼出通用分享组件问题

--- src/UCAndroidBrowser.js
+++ src/UCAndroidBrowser.js
@@ -5,20 +5,20 @@ const commamdMap = {
     appMessage: 'WechatFriends',
     qq: 'QQ',
     weiBo: 'SinaWeibo',
-    default: undefined,
+    default: '',
 }
 
 function callShare(command = 'default', options = {}) {
     setShareData(options)
     const toApp = commamdMap[command]
     ucweb.startRequest('shell.page_share', [
-        shareData.desc,
         shareData.title,
+        shareData.desc,
         shareData.link,
         toApp,
         '',
         shareData.from,
-        '',
+        shareData.icon,
     ])
 }
 
