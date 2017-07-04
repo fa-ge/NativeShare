e936b75f3bfba7abc3a91477fc8ba6136292e338
fage <924053302@qq.com>
2017-07-05 01:55:52 +0800

    增加android QQ分享

---
+++ src/QQAndroid.js
@@ -0,0 +1,44 @@
+import { loadJs, getHostnameFromUrl } from './utils'
+import Share from './Share'
+
+export default class QQAndroid extends Share {
+    constructor(config) {
+        super(config)
+        this.init()
+    }
+
+    setShareData(options) {
+        super.setShareData(options)
+        const shareData = this.getShareData()
+        if (getHostnameFromUrl(shareData.link) !== location.hostname) {
+            shareData.link = location.href
+            console.warn('安卓的QQ自带浏览器分享url必须跟页面url同一个域名，已自动为你设置为当前页面的url')
+        }
+        try {
+            mqq.data.setShareInfo(
+                {
+                    share_url: shareData.link,
+                    title: shareData.title,
+                    desc: shareData.desc,
+                    image_url: shareData.icon,
+                },
+                success => {
+                    if (!success) {
+                        console.warn('文案设置出错')
+                    }
+                }
+            )
+        } catch (err) {}
+    }
+
+    call(command = 'default', options) {
+        this.setShareData(options)
+        mqq.ui.showShareMenu()
+    }
+
+    init() {
+        loadJs('https://open.mobile.qq.com/sdk/qqapi.js', () => {
+            this.setShareData()
+        })
+    }
+}
---
+++ src/QQIos.js
@@ -0,0 +1,42 @@
+import { loadJs } from './utils'
+import Share from './Share'
+
+export default class QQ extends Share {
+    constructor(config) {
+        super(config)
+        this.init()
+    }
+
+    call(command = 'default', options) {
+        this.setShareData(options)
+        mqq.ui.showShareMenu()
+    }
+
+    init() {
+        loadJs('https://open.mobile.qq.com/sdk/qqapi.js', () => {
+            const shareData = this._shareData
+            mqq.ui.setOnShareHandler(function(type) {
+                mqq.ui.shareMessage(
+                    {
+                        back: true,
+                        share_type: type,
+                        title: shareData.title,
+                        desc: shareData.desc,
+                        share_url: shareData.link,
+                        image_url: shareData.icon,
+                        sourceName: shareData.from,
+                    },
+                    function({ retCode }) {
+                        if (retCode === 0) {
+                            shareData.success.call(this)
+                        } else if (retCode === 1) {
+                            shareData.cancel.call(this)
+                        } else {
+                            shareData.fail.call(this)
+                        }
+                    }
+                )
+            })
+        })
+    }
+}
